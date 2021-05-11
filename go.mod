788b14354aeee6ef252da2f269b0102a7b6ae27c
gaohua <gaohuacode@gmail.com>
2021-05-11 16:42:38 +0800

    completed http.go

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/gaohua/proglog
 
 go 1.16
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- internal/server/http.go
+++ internal/server/http.go
@@ -10,8 +10,8 @@ import (
 func NewHTTPServer(addr string) *http.Server {
 	httpsrv := newHTTPServer()
 	r := mux.NewRouter()
-	r.HandleFunc("/", httpsrv.handleProduce).Method("POST")
-	r.HandleFunc("/", httpsrv.handleProduce).Method("POST")
+	r.HandleFunc("/", httpsrv.handleProduce).Methods("POST")
+	r.HandleFunc("/", httpsrv.handleConsume).Methods("GET")
 
 	return &http.Server{
 		Addr:    addr,
@@ -59,6 +59,30 @@ func (s *httpServer) handleProduce(w http.ResponseWriter, r *http.Request) {
 	}
 
 	res := ProduceResponse{Offset: off}
+	err = json.NewEncoder(w).Encode(res)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (s *httpServer) handleConsume(w http.ResponseWriter, r *http.Request) {
+	var req ConsumeRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	record, err := s.Log.Read(req.Offset)
+	if err == ErrOffsetNotFound {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	res := ConsumeResponse{Record: record}
 	err = json.NewEncoder(w).Encode(res)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
